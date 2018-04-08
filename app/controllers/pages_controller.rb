60217c75eea0bcff4f76135d17e7769ee008b74c
nguyentoan42 <toand@atmarkcafe.org>
2018-04-08 10:07:10 +0700

    devise

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -1,5 +1,4 @@
 class PagesController < ApplicationController 
-
   def index
     @categories = Category.all.order("id desc")
     @products = Product.all.order("id desc")
---
+++ config/initializers/devise.rb
@@ -0,0 +1,31 @@
+Devise.setup do |config|
+
+  config.mailer_sender = 'AppRails <ductoannguyen04@gmail.com>'
+
+  require 'devise/orm/active_record'
+
+  config.secret_key = '9c14551f28d0e75224b881c616002e4ece67f3b34355bff66f242efa2e19dc4d9a3703786beedcc890f5463657fcffce686f997dddf8d075e7c52e3af980a8a3'
+
+  config.scoped_views = true
+
+  config.case_insensitive_keys = [:email]
+  config.strip_whitespace_keys = [:email]
+  config.skip_session_storage = [:http_auth]
+  config.stretches = Rails.env.test? ? 1 : 10
+
+  config.reconfirmable = false
+  config.expire_all_remember_me_on_sign_out = true
+  config.password_length = 6..128
+  config.reset_password_within = 6.hours
+  config.sign_out_via = :delete
+
+  config.timeout_in = 10.days
+
+  config.lock_strategy = :failed_attempts
+  config.unlock_keys = [:email]
+  config.unlock_strategy = :both
+  config.maximum_attempts = 5
+  config.unlock_in = 10.minutes
+  config.last_attempt_warning = true
+  
+end
