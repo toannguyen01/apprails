1e360cc27bdd66737e106e4051c1da5bc067d531
nguyentoan42 <toand@atmarkcafe.org>
2018-04-08 16:17:39 +0700

    update info user

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,8 @@
 module ApplicationHelper
+  require 'uri'
+
+  def avatar_url(user)
+    gravatar_id = Digest::MD5::hexdigest(user.email).downcase
+    "https://www.gravatar.com/avatar/#{gravatar_id}.jpg?d=identicon&s=25"
+  end
 end
