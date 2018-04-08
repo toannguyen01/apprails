c2ac5452bae530dc8ed315555f5a34bb7ec532c3
nguyentoan42 <toand@atmarkcafe.org>
2018-04-08 20:54:46 +0700

    list product with order

--- app/controllers/categories_controller.rb
+++ app/controllers/categories_controller.rb
@@ -1,4 +1,17 @@
-class CategoriesController < ApplicationCotroller 
-  
+class CategoriesController < ApplicationController 
+  before_action :get_categories, only: [:index, :show]
+  def index
+    @products = Product.all.order("id desc")
+  end
 
+  def show    
+    category = Category.find(params[:id])
+    @products =category.products.order("id desc")
+  end
+
+  private
+
+  def get_categories
+    @categories = Category.all.order("id desc")
+  end
 end
