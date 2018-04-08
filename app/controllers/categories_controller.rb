deb66f461349e4f06e92935cc7d7aa896fdd5cf8
nguyentoan42 <toand@atmarkcafe.org>
2018-04-08 21:06:18 +0700

    list product with pagination

--- app/controllers/categories_controller.rb
+++ app/controllers/categories_controller.rb
@@ -1,12 +1,12 @@
 class CategoriesController < ApplicationController 
   before_action :get_categories, only: [:index, :show]
   def index
-    @products = Product.all.order("id desc")
+    @products = Product.all.order("id desc").page(params[:page]).per(6)
   end
 
   def show    
     category = Category.find(params[:id])
-    @products =category.products.order("id desc")
+    @products =category.products.order("id desc").page(params[:page]).per(6)
   end
 
   private
--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -1,7 +1,7 @@
 class PagesController < ApplicationController 
   def index
     @categories = Category.all.order("id desc")
-    @products = Product.all.order("id desc")
+    @products = Product.all.order("id desc").page(params[:page]).per(6)
   end
 
 end
